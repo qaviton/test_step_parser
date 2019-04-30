324366eaf5e62dfeb58c3e82237094f6c7117bad
qaviton <qaviton@gmail.com>
2019-04-30 13:52:43 +0300

    updated case for invalid steps file

--- py/test_step_parser/__init__.py
+++ py/test_step_parser/__init__.py
@@ -25,7 +25,7 @@ steps_file = 'stepsX.json'
 pattern = "*steps.json"
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 # TODO: dynamic steps.json creation using Antlr for every function with :step: in its docstring
@@ -41,8 +41,11 @@ def create_steps(*,
             if fnmatch(name, pattern):
                 file = os.path.join(path, name)
                 with open(file) as steps_file:
-                    for project in json.load(steps_file):
-                        steps.append(project)
+                    try:
+                        for project in json.load(steps_file):
+                            steps.append(project)
+                    except:
+                        pass
 
     with open(save, mode) as fp:
         json.dump(steps, fp, indent=2)
